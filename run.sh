ff14e556b37d3a8ee5492f257148739ec0577221
Sunhong Min <sunhongmin10@gmail.com>
2022-09-30 13:35:35 +0900

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+cgexec -g memory:64gb python3 -W ignore run_baseline.py --dataset ogbn_papers_extended --sb-size 3300 --num-epochs 1 --train-only
+cgexec -g memory:64gb python3 -W ignore run_baseline.py --dataset friendster_extended --sb-size 3600 --num-epochs 1 --train-only
+cgexec -g memory:64gb python3 -W ignore run_baseline.py --dataset ogbn_products_extended --sb-size 2100 --num-epochs 1 --train-only
+cgexec -g memory:64gb python3 -W ignore run_baseline.py --dataset twitter_extended --sb-size 6400 --num-epochs 1 --train-only
+
+cgexec -g memory:64gb python3 -W ignore run_ginex.py --dataset ogbn_papers_extended --sb-size 3300 --neigh-cache-size 45000000000 --feature-cache-size 50000000000 --num-epochs 1 --verbose --train-only
+cgexec -g memory:64gb python3 -W ignore run_ginex.py --dataset friendster_extended --sb-size 3600 --neigh-cache-size 45000000000 --feature-cache-size 50000000000 --num-epochs 1 --verbose --train-only
+cgexec -g memory:64gb python3 -W ignore run_ginex.py --dataset ogbn_products_extended --sb-size 2100 --neigh-cache-size 45000000000 --feature-cache-size 50000000000 --num-epochs 1 --verbose --train-only
+cgexec -g memory:64gb python3 -W ignore run_ginex.py --dataset twitter_extended --sb-size 6400 --neigh-cache-size 45000000000 --feature-cache-size 50000000000 --num-epochs 1 --verbose --train-only
