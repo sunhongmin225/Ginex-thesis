6b05696b8eb032a196590d55ff1403c18c7b25a8
Sunhong Min <sunhongmin@snu.ac.kr>
2022-10-07 20:08:52 +0900

    Add cmd for papers (not extended)

--- run.sh
+++ run.sh
@@ -7,3 +7,5 @@ cgexec -g memory:64gb python3 -W ignore run_ginex.py --dataset ogbn_papers_exten
 cgexec -g memory:64gb python3 -W ignore run_ginex.py --dataset friendster_extended --sb-size 3600 --neigh-cache-size 45000000000 --feature-cache-size 50000000000 --num-epochs 1 --verbose --train-only
 cgexec -g memory:64gb python3 -W ignore run_ginex.py --dataset ogbn_products_extended --sb-size 2100 --neigh-cache-size 45000000000 --feature-cache-size 50000000000 --num-epochs 1 --verbose --train-only
 cgexec -g memory:64gb python3 -W ignore run_ginex.py --dataset twitter_extended --sb-size 6400 --neigh-cache-size 45000000000 --feature-cache-size 50000000000 --num-epochs 1 --verbose --train-only
+
+sudo PYTHONPATH=/home/sunhong/.local/lib/python3.8/site-packages python3 -W ignore run_ginex.py --sb-size 2100 --num-epochs 1 --neigh-cache-size 15000000000 --feature-cache-size 6000000000 --verbose --train-only --khop 1
