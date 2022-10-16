141ed175b365d5ba82b65cd60e3269f533c81434
Sunhong Min <sunhongmin10@gmail.com>
2022-10-16 19:18:38 +0900

    Add update_khop (not completed)

--- lib/cpp_extension/update.cpp
+++ lib/cpp_extension/update.cpp
@@ -35,8 +35,33 @@ void cache_update(torch::Tensor cache, torch::Tensor address_table, torch::Tenso
         return;
 }
 
+void cache_update_khop(torch::Tensor cache, torch::Tensor address_table, torch::Tensor batch_inputs_1, torch::Tensor batch_inputs_2, torch::Tensor in_indices, torch::Tensor in_positions, torch::Tensor out_indices, int64_t num_features){
+
+        auto cache_data = cache.data_ptr<float>();
+        auto address_table_data = address_table.data_ptr<int32_t>();
+        auto batch_inputs_1_data = batch_inputs_1.data_ptr<float>();
+        auto batch_inputs_2_data = batch_inputs_2.data_ptr<float>();
+        auto in_indices_data = in_indices.data_ptr<int64_t>();
+        auto in_positions_data = in_positions.data_ptr<int32_t>();
+        auto out_indices_data = out_indices.data_ptr<int64_t>();
+
+        int64_t num_idx = in_indices.numel();
+        int64_t feature_size = num_features*sizeof(float);
+
+        #pragma omp parallel for num_threads(torch::get_num_threads())
+        for (int64_t n = 0; n < num_idx; n++) {
+                int32_t cache_out_idx = address_table_data[out_indices_data[n]];
+                memcpy(cache_data+num_features*cache_out_idx, batch_inputs_1_data+num_features*in_positions_data[n], feature_size);
+                address_table_data[in_indices_data[n]] = cache_out_idx;
+                address_table_data[out_indices_data[n]] = -1;
+        }
+
+        return;
+}
+
 PYBIND11_MODULE(update, m) {
     m.def("cache_update", &cache_update, "evict & insert cache entries with the given indices");
+    m.def("cache_update_khop", &cache_update_khop, "evict & insert cache entries with the given indices for khop approximation");
 }
 
 
