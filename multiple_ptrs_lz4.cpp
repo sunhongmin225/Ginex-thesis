28fd24928f90558e8cb813fb9aa485a58e9fb540
Sunhong Min <sunhongmin10@gmail.com>
2022-11-02 15:32:06 +0900

    Add lz4 toy examples and update gitignre

---
+++ multiple_ptrs_lz4.cpp
@@ -0,0 +1,166 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+extern "C" {
+    #include "lz4.h"
+}
+#include <vector>
+
+void run_screaming(const char* message, const int code) {
+  printf("%s \n", message);
+  exit(code);
+}
+
+int main(void) {
+
+  // Data generation
+  int num_elems_0 = 5;
+  int64_t* src_int64_0 = (int64_t*) malloc(num_elems_0 * sizeof(int64_t));
+  src_int64_0[0] = 912309;
+  src_int64_0[1] = -334234;
+  src_int64_0[2] = 192933;
+  src_int64_0[3] = 231233;
+  src_int64_0[4] = -543423;
+
+  int num_elems_1 = 4;
+  int64_t* src_int64_1 = (int64_t*) malloc(num_elems_1 * sizeof(int64_t));
+  src_int64_1[0] = 6;
+  src_int64_1[1] = 2;
+  src_int64_1[2] = 7;
+  src_int64_1[3] = -1;
+
+  int num_elems_2 = 6;
+  int64_t* src_int64_2 = (int64_t*) malloc(num_elems_2 * sizeof(int64_t));
+  src_int64_2[0] = -612;
+  src_int64_2[1] = 734;
+  src_int64_2[2] = 713;
+  src_int64_2[3] = 833;
+  src_int64_2[4] = -423;
+  src_int64_2[5] = 163;
+
+  std::vector<int> num_elems;
+  num_elems.push_back(num_elems_0);
+  num_elems.push_back(num_elems_1);
+  num_elems.push_back(num_elems_2);
+
+  std::vector<int64_t*> src_int64;
+  src_int64.push_back(src_int64_0);
+  src_int64.push_back(src_int64_1);
+  src_int64.push_back(src_int64_2);
+
+  for (int i = 0; i < num_elems.size(); i++) {
+    for (int j = 0; j < num_elems[i]; j++) {
+      printf("src_int64_%d[%d] = %ld\n", i, j, src_int64[i][j]);
+    }
+    printf("\n");
+  }
+
+  size_t ret;
+  std::vector<int64_t*> compressed_datas;
+  std::vector<int> src_sizes;
+  std::vector<int> compressed_data_sizes;
+
+  /*
+  // Compression
+  for (int i = 0; i < num_elems.size(); i++) {
+    int src_size = num_elems[i] * sizeof(int64_t);
+    printf("src_size[%d] in bytes = %d\n", i, src_size);
+    int max_dst_size = LZ4_compressBound(src_size);
+    printf("max_dst_size[%d] in bytes = %d\n", i, max_dst_size);
+    int64_t* compressed_data = (int64_t*) malloc((size_t) max_dst_size);
+    if (compressed_data == NULL)
+      run_screaming("Failed to allocate memory for *compressed_data.", 1);
+
+    int compressed_data_size = LZ4_compress_default((const char*) src_int64[i], (char*) compressed_data, src_size, max_dst_size);
+    printf("compressed_data_size[%d] in bytes = %d\n", i, compressed_data_size);
+    if (compressed_data_size <= 0)
+      run_screaming("A 0 or negative result from LZ4_compress_default() indicates a failure trying to compress the data. ", 1);
+    if (compressed_data_size > 0)
+      printf("We successfully compressed some data for i = %d. Ratio: %.2f\n\n", i, (float) compressed_data_size/src_size);
+
+    compressed_data = (int64_t *) realloc(compressed_data, (size_t) compressed_data_size);
+    if (compressed_data == NULL)
+      run_screaming("Failed to re-alloc memory for compressed_data.  Sad :(", 1);
+
+    compressed_datas.push_back(compressed_data);
+    compressed_data_sizes.push_back(compressed_data_size);
+  }
+
+  // Save as file
+  FILE *fw;
+  fw = fopen("toy_lz4.dat.lz4", "wb"); // "a"
+  for (int i = 0; i < num_elems.size(); i++) {
+    ret = fwrite(compressed_datas[i], 1, compressed_data_sizes[i], fw);
+  }
+  fclose(fw);
+  printf("arcmsh::successfully wrote file will ret = %ld\n", ret);
+  */
+
+  // Read from file
+  FILE *fr;
+  compressed_data_sizes.push_back(35);
+  compressed_data_sizes.push_back(23);
+  compressed_data_sizes.push_back(37);
+  src_sizes.push_back(num_elems_0 * sizeof(int64_t));
+  src_sizes.push_back(num_elems_1 * sizeof(int64_t));
+  src_sizes.push_back(num_elems_2 * sizeof(int64_t));
+  int entire_compressed_buffer_size = 0;
+  for (int i = 0; i < num_elems.size(); i++) {
+    entire_compressed_buffer_size += compressed_data_sizes[i];
+  }
+  int64_t* entire_compressed_buffer = (int64_t*) malloc(entire_compressed_buffer_size);
+  fr = fopen("toy_lz4.dat.lz4", "rb"); // "a"
+  ret = fread(entire_compressed_buffer, 1, entire_compressed_buffer_size, fr);
+  fclose(fr);
+  printf("arcmsh::successfully read file will ret = %ld\n", ret);
+
+  // Decompression
+  int64_t offset = 0;
+  for (int i = 0; i < num_elems.size(); i++) {
+    int64_t* regen_buffer = (int64_t*) malloc(src_sizes[i]);
+    int decompressed_size = LZ4_decompress_safe((char*) (entire_compressed_buffer) + offset, (char*) regen_buffer, compressed_data_sizes[i], src_sizes[i]);
+    printf("decompressed_size in bytes = %d\n", decompressed_size);
+    offset += compressed_data_sizes[i];
+    for (int j = 0; j < num_elems[i]; j++) {
+      printf("src_int64[%d][%d] = %ld\n", i, j, src_int64[i][j]);
+      printf("regen_buffer[%d]  = %ld\n", j, regen_buffer[j]);
+    }
+    if (memcmp(src_int64[i], regen_buffer, src_sizes[i]) != 0)
+      run_screaming("Validation failed.  *src and *new_src are not identical.", 1);
+    printf("Validation done for i = %d.\n", i);
+    free(regen_buffer);
+  }
+
+  // int64_t* const regen_buffer = (int64_t*) malloc(src_size);
+  // if (regen_buffer == NULL)
+  //   run_screaming("Failed to allocate memory for *regen_buffer.", 1);
+  
+  // const int decompressed_size = LZ4_decompress_safe((char*) comp_buffer, (char*) regen_buffer, compressed_data_size, src_size);
+  // printf("decompressed_size in bytes = %d\n", decompressed_size);
+  // free(compressed_data);
+  // free(comp_buffer);
+  // if (decompressed_size < 0)
+    // run_screaming("A negative result from LZ4_decompress_safe indicates a failure trying to decompress the data.  See exit code (echo $?) for value returned.", decompressed_size);
+  // if (decompressed_size >= 0)
+    // printf("We successfully decompressed some data!\n");
+  // if (decompressed_size != src_size)
+    // run_screaming("Decompressed data is different from original! \n", 1);
+
+  // // Validation
+  // for (int i = 0; i < num_elems; i++) {
+  //   printf("   src_int64[%d] = %ld\n", i, src_int64[i]);
+  //   printf("regen_buffer[%d] = %ld\n", i, regen_buffer[i]);
+  // }
+  // if (memcmp(src_int64, regen_buffer, src_size) != 0)
+  //   run_screaming("Validation failed.  *src and *new_src are not identical.", 1);
+  // printf("Validation done.\n");
+
+  // Free
+  for (int i = 0; i < num_elems.size(); i++) {
+    free(src_int64[i]);
+    // free(compressed_datas[i]);
+  }
+  free(entire_compressed_buffer);
+
+  return 0;
+}
---
+++ single_ptr_lz4.cpp
@@ -0,0 +1,146 @@
+#include <stdio.h>   // For printf()
+#include <string.h>  // For memcmp()
+#include <stdlib.h>  // For exit()
+extern "C" {
+    #include "lz4.h" // This is all that is required to expose the prototypes for basic compression and decompression.
+}
+
+/*
+ * Simple show-error-and-bail function.
+ */
+void run_screaming(const char* message, const int code) {
+  printf("%s \n", message);
+  exit(code);
+}
+
+/*
+ * main
+ */
+int main(void) {
+  /* Introduction */
+  // Below we will have a Compression and Decompression section to demonstrate.
+  // There are a few important notes before we start:
+  //   1) The return codes of LZ4_ functions are important.
+  //      Read lz4.h if you're unsure what a given code means.
+  //   2) LZ4 uses char* pointers in all LZ4_ functions.
+  //      This is baked into the API and not going to change, for consistency.
+  //      If your program uses different pointer types,
+  //      you may need to do some casting or set the right -Wno compiler flags to ignore those warnings (e.g.: -Wno-pointer-sign).
+
+  /* Compression */
+  // We'll store some text into a variable pointed to by *src to be compressed later.
+  // const char* const src = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor site amat.";
+  int num_elems = 7;
+  int64_t* src_int64 = (int64_t*) malloc(num_elems * sizeof(int64_t));
+  src_int64[0] = 912309;
+  src_int64[1] = 334234;
+  src_int64[2] = 192933;
+  src_int64[3] = 231233;
+  src_int64[4] = 543423;
+  src_int64[5] = 623123;
+  src_int64[6] = 822342;
+  for (int i = 0; i < num_elems; i++)
+    printf("src_int64[%d] = %ld\n", i, src_int64[i]);
+
+  // The compression function needs to know how many bytes exist.  Since we're using a string, we can use strlen() + 1 (for \0).
+  // const int src_size = (int)(strlen(src) + 1);
+  const int src_size = num_elems * sizeof(int64_t);
+  printf("src_size in bytes = %d\n", src_size);
+
+  // LZ4 provides a function that will tell you the maximum size of compressed output based on input data via LZ4_compressBound().
+  // const int max_dst_size = LZ4_compressBound(src_size);
+  const int max_dst_size = LZ4_compressBound(src_size);
+  printf("max_dst_size in bytes = %d\n", max_dst_size);
+
+  // We will use that size for our destination boundary when allocating space.
+  // char* compressed_data = (char*) malloc((size_t)max_dst_size);
+  int64_t* compressed_data = (int64_t*) malloc((size_t)max_dst_size);
+  if (compressed_data == NULL)
+    run_screaming("Failed to allocate memory for *compressed_data.", 1);
+
+  // That's all the information and preparation LZ4 needs to compress *src into* compressed_data.
+  // Invoke LZ4_compress_default now with our size values and pointers to our memory locations.
+  // Save the return value for error checking.
+  const int compressed_data_size = LZ4_compress_default((const char*) src_int64, (char*) compressed_data, src_size, max_dst_size);
+  printf("compressed_data_size in bytes = %d\n", compressed_data_size);
+  // Check return_value to determine what happened.
+  if (compressed_data_size <= 0)
+    run_screaming("A 0 or negative result from LZ4_compress_default() indicates a failure trying to compress the data. ", 1);
+  if (compressed_data_size > 0)
+    printf("We successfully compressed some data! Ratio: %.2f\n", (float) compressed_data_size/src_size);
+
+  // Not only does a positive return_value mean success, the value returned == the number of bytes required.
+  // You can use this to realloc() *compress_data to free up memory, if desired.  We'll do so just to demonstrate the concept.
+  // compressed_data = (char *)realloc(compressed_data, (size_t)compressed_data_size);
+  compressed_data = (int64_t *) realloc(compressed_data, (size_t)compressed_data_size);
+  if (compressed_data == NULL)
+    run_screaming("Failed to re-alloc memory for compressed_data.  Sad :(", 1);
+
+  /* Save as file */
+  size_t ret;
+  FILE *fw;
+  fw = fopen("toy_lz4.dat.lz4", "wb"); // "a"
+  ret = fwrite(compressed_data, 1, compressed_data_size, fw);
+
+  fclose(fw);
+
+  printf("arcmsh::successfully wrote file will ret = %ld\n", ret);
+
+  /* Read from file*/
+  FILE *fr;
+  int64_t* comp_buffer = (int64_t*) malloc(compressed_data_size);
+  fr = fopen("toy_lz4.dat.lz4", "rb"); // "a"
+  ret = fread(comp_buffer, 1, compressed_data_size, fr);
+  printf("arcmsh::read file with ret = %ld\n", ret);
+  fclose(fr);
+
+  printf("arcmsh::successfully read file\n");
+
+  /* Decompression */
+  // Now that we've successfully compressed the information from *src to *compressed_data, let's do the opposite!
+  // The decompression will need to know the compressed size, and an upper bound of the decompressed size.
+  // In this example, we just re-use this information from previous section,
+  // but in a real-world scenario, metadata must be transmitted to the decompression side.
+  // Each implementation is in charge of this part. Oftentimes, it adds some header of its own.
+  // Sometimes, the metadata can be extracted from the local context.
+
+  // First, let's create a *new_src location of size src_size since we know that value.
+  // char* const regen_buffer = (char*)malloc(src_size);
+  int64_t* const regen_buffer = (int64_t*) malloc(src_size);
+  if (regen_buffer == NULL)
+    run_screaming("Failed to allocate memory for *regen_buffer.", 1);
+  
+  // The LZ4_decompress_safe function needs to know where the compressed data is, how many bytes long it is,
+  // where the regen_buffer memory location is, and how large regen_buffer (uncompressed) output will be.
+  // Again, save the return_value.
+  // const int decompressed_size = LZ4_decompress_safe((char*) compressed_data, (char*) regen_buffer, compressed_data_size, src_size);
+  // const int decompressed_size = LZ4_decompress_safe((char*) compressed_data, (char*) regen_buffer, compressed_data_size, src_size);
+  const int decompressed_size = LZ4_decompress_safe((char*) comp_buffer, (char*) regen_buffer, compressed_data_size, src_size);
+  // const int decompressed_size = LZ4_decompress_safe_partial((char*) (compressed_data), (char*) regen_buffer, src_size, target_output_size, src_size);
+  printf("decompressed_size in bytes = %d\n", decompressed_size);
+  free(compressed_data);   /* no longer useful */
+  free(comp_buffer);
+  if (decompressed_size < 0)
+    run_screaming("A negative result from LZ4_decompress_safe indicates a failure trying to decompress the data.  See exit code (echo $?) for value returned.", decompressed_size);
+  if (decompressed_size >= 0)
+    printf("We successfully decompressed some data!\n");
+  // Not only does a positive return value mean success,
+  // value returned == number of bytes regenerated from compressed_data stream.
+  if (decompressed_size != src_size)
+    run_screaming("Decompressed data is different from original! \n", 1);
+
+  /* Validation */
+  // We should be able to compare our original *src with our *new_src and be byte-for-byte identical.
+  for (int i = 0; i < num_elems; i++) {
+    printf("   src_int64[%d] = %ld\n", i, src_int64[i]);
+    printf("regen_buffer[%d] = %ld\n", i, regen_buffer[i]);
+  }
+  if (memcmp(src_int64, regen_buffer, src_size) != 0)
+    run_screaming("Validation failed.  *src and *new_src are not identical.", 1);
+  printf("Validation done.\n");
+
+  free(src_int64);
+  free(regen_buffer);
+
+  return 0;
+}
